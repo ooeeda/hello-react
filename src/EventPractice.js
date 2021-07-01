24fde730433ccb02bbb0d566dd00285462596b96
박수환 <ooeeda@gmail.com>
2021-07-01 17:12:23 +0900

    4.2.3 함수밖으로빼기

--- src/EventPractice.js
+++ src/EventPractice.js
@@ -6,24 +6,34 @@ class EventPractice extends Component{
         message : ''
     }
 
+    constructor(props){
+        super(props)
+
+        this.handleChange  = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleChange(e){
+        this.setState({
+            message : e.target.value
+        });
+    }
+
+    handleClick(){
+        alert(this.state.message);
+        this.setState({
+            message : ''
+        });
+    }
+
     render(){
         return (
             <div>
                 <h1>연습 : {this.state.message}</h1>
                 <input type="text" name="message" placeholder="아무거나입력" value={this.state.message}
-                       onChange={ (e) => {
-                           this.setState({
-                               message : e.target.value
-                           })
-                       }}
+                       onChange={this.handleChange}
                 />
-                <button onClick={()=>{
-                   alert(this.state.message);
-                   this.setState({
-                       message : ''
-                   });
-                }}
-                >연습확인</button>
+                <button onClick={this.handleClick}>연습확인</button>
             </div>
         );
     }
