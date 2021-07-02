cca24c023587ac962a23bb9396af2bdd011d3ef1
박수환 <ooeeda@gmail.com>
2021-07-02 10:35:09 +0900

    4.4 function방식에서 taget.name사용을 위한 form 사용법

--- src/EventPracticeFnc.js
+++ src/EventPracticeFnc.js
@@ -2,16 +2,39 @@ import React, {useState} from 'react';
 
 const EventPracticeFnc = () => {
 
-    const [username, setUsername] = useState('');
+    /*const [username, setUsername] = useState('');
     const [message, setMessage] = useState('');
-
     const onChangeUserName = e => setUsername(e.target.value);
     const onChangeMessage = e => setMessage(e.target.value);
-
     const onClick =()=>{
         alert(username+ ' : '+message);
         setUsername('');
         setMessage('');
+    };*/
+
+    const [form, setForm] = useState(
+        {
+            username: '',
+            message: ''
+        }
+    );
+    const {username, message} = form;
+
+
+    const onChange = e =>{
+        const nextForm = {
+            ...form,
+            [e.target.name] : e.target.value
+        };
+        setForm(nextForm);
+    };
+
+    const onClick =()=>{
+        alert(username+ ' : '+message);
+        setForm({
+            username : '',
+            message : ''
+        });
     };
 
     const onKeyPress = e =>{
@@ -25,10 +48,10 @@ const EventPracticeFnc = () => {
         <div>
             <h1>function 방식으로 이벤트 호출</h1>
             <input type='text' name="username" placeholder="사용자명" value={username}
-                   onChange={onChangeUserName} onKeyPress={onKeyPress}
+                   onChange={onChange} onKeyPress={onKeyPress}
             />
             <input type='text' name='message' placeholder="내용" value={message}
-                   onChange={onChangeMessage} onKeyPress={onKeyPress}
+                   onChange={onChange} onKeyPress={onKeyPress}
 
             />
             <button onClick={onClick}>확인</button>
